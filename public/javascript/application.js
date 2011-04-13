0942ac9e51ade5fdb8ccb48b7f125c7651967364
Aron Carroll <self@aroncarroll.com>
2011-04-13 23:27:13 +0100

    Added basic Task and TaskCollection obejcts

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,24 @@
-/*jshint immed:false */
-(function (Backbone, _, document, window, undefined) {
-  
-})(this.Backbone, this._, this.document, this);
+/*jshint immed:false, onevar: false */
+(function (Backbone, _, $, document, window, undefined) {
+
+  var Task = Backbone.Model.extend({
+    constructor: function Task() {
+      Backbone.Model.prototype.constructor.apply(this, arguments);
+    }
+  });
+
+  var TaskCollection = Backbone.Collection.extend({
+    url: '/tasks',
+
+    model: Task,
+
+    constructor: function TaskCollection() {
+      Backbone.Collection.prototype.constructor.apply(this, arguments);
+    }
+  });
+
+  window.Task = Task;
+  window.TaskCollection = TaskCollection;
+  window.tasks = new TaskCollection(bootstrap());
+
+})(this.Backbone, this._, this.Zepto, this.document, this);
